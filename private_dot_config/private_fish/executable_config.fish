230ffe5b05062bb32a553a2af72dbd560d6cb875
音㦡 <215371699+med0rei@users.noreply.github.com>
2026-01-21 15:20:11 +0900

    add

---
+++ private_dot_config/private_fish/executable_config.fish
@@ -0,0 +1,192 @@
+#!/usr/bin/fish
+
+# chezmoi cdの場合はcdしない
+if not set -q CHEZMOI_SOURCE_DIR
+  cd ~/
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+# source ~/.orbstack/shell/init.zsh 2>/dev/null || :
+
+
+# peco function fish_user_key_bindings
+bind \cr peco_select_history    # Ctrl + r
+
+# Fish shell
+set -g fish_prompt_pwd_dir_length      0
+set -g theme_color_scheme              dracula
+set -g theme_display_cmd_duration      no
+set -g theme_display_git_master_branch yes
+set -g theme_newline_cursor            yes
+
+# Prompt separator line between cwd and input
+set -g theme_newline_prompt (begin; set_color --bold brcyan; printf '%s' 'Eclipse first, the rest nowhere.\n'; set_color normal; printf '\n'; set_color --bold brcyan; printf '%s' '> '; set_color normal; end)
+
+# xdg
+set -gx XDG_DATA_HOME   $HOME/.local/share
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_STATE_HOME  $HOME/.local/state
+set -gx XDG_CACHE_HOME  $HOME/.cache
+set -gx XDG_RUNTIME_DIR $TMPDIR
+
+# bash
+set -gx HISTFILE $XDG_STATE_HOME/bash/history
+
+# GPG
+set -gx GPG_TTY   $(tty)
+set -gx GNUPGHOME $XDG_DATA_HOME/gnupg
+
+# Homebrew
+# eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+
+# Editor
+set -gx EDITOR nvim
+
+# Vim
+# set -gx VIM $XDG_CONFIG_HOME/vim
+
+# Binary files
+fish_add_path /usr/bin/
+
+
+# Wine
+set -gx WINEPREFIX $XDG_DATA_HOME/wine
+
+
+# JetBrains
+# Added by Toolbox App
+fish_add_path $HOME/Library/Application Support/JetBrains/Toolbox/scripts
+
+
+# Rust
+set -gx CARGO_HOME  $XDG_DATA_HOME/cargo
+set -gx RUSTUP_HOME $XDG_DATA_HOME/rustup
+fish_add_path $CARGO_HOME/bin $rust_path
+
+# Python
+set -gx IPYTHONDIR        $XDG_CONFIG_HOME/ipython
+set -gx JUPYTER_CONFIG_DIR $XDG_CONFIG_HOME/jupyter
+set -gx PYTHONSTARTUP     $XDG_CONFIG_HOME/python/pythonrc
+set -gx MPLCONFIGDIR      $XDG_CONFIG_HOME/matplotlib
+set -gx PYTHON_HISTORY $XDG_STATE_HOME/python/history
+fish_add_path $HOME/.local/lib/python3.8/site-packages
+fish_add_path /usr/lib/python3/dist-packages
+fish_add_path $HOME/Library/Python/3.9/bin
+
+# Ruby
+fish_add_path $HOME/.rvm/bin
+# rvm default
+
+# Yarn
+fish_add_path $HOME/.yarn/bin
+
+# Go
+set -gx GOPATH $XDG_DATA_HOME/go
+fish_add_path /usr/local/go/bin
+fish_add_path $GOPATH
+fish_add_path $GOPATH/bin
+
+# pipx
+set PATH $PATH $HOME/.local/bin
+
+# npm
+set -gx NPM_CONFIG_INIT_MODULE $XDG_CONFIG_HOME/npm/config/npm-init.js
+set -gx NPM_CONFIG_CACHE       $XDG_CACHE_HOME/npm
+set -gx NPM_CONFIG_TMP         $XDG_RUNTIME_DIR/npm
+
+# pnpm
+set -gx PNPM_HOME $HOME/.local/share/pnpm
+# Nix版pnpmを使用するため、PNPM_HOME自体はPATHに追加しない
+# グローバルパッケージのbinディレクトリのみPATHに追加
+fish_add_path $PNPM_HOME/node_modules/.bin
+
+# nodejs
+set -gx NODE_REPL_HISTORY $XDG_STATE_HOME/node_repl_history
+
+# gradle
+set -gx GRADLE_USER_HOME $XDG_DATA_HOME/gradle
+
+# maven
+alias mvn "mvn -gs $XDG_CONFIG_HOME/maven/settings.xml"
+
+# wakatime
+set -gx WAKATIME_HOME $XDG_CONFIG_HOME/wakatime
+
+# dotnet
+set -gx DOTNET_CLI_HOME $XDG_DATA_HOME/dotnet
+
+# php
+set -gx COMPOSER_HOME $XDG_CONFIG_HOME/composer
+fish_add_path $COMPOSER_HOME/vendor/bin
+
+# sonarlint
+set -gx SONARLINT_USER_HOME $XDG_DATA_HOME/sonarlint
+
+# android
+set -gx ANDROID_USER_HOME $XDG_DATA_HOME/android
+alias adb 'HOME="$XDG_DATA_HOME"/android adb'
+
+# docker
+set -gx DOCKER_CONFIG $XDG_CONFIG_HOME/docker
+
+# less
+set -gx LESSHISTFILE $XDG_STATE_HOME/less/history
+
+# zsh
+set -gx ZDOTDIR $XDG_CONFIG_HOME/zsh
+
+# Aliases
+alias b        "cd .."
+alias br       "brew"
+alias c        "cd"
+alias ccu      "pnpm dlx ccusage"
+alias cl       "claude"
+alias cm       "chezmoi"
+alias cmcd     "cd ~/.local/share/chezmoi"
+alias cdx      "codex"
+alias co       "cargo"
+alias f        "fish"
+alias fishconf "cd ~/.config/fish && nvim config.fish"
+alias g        "git"
+alias gg       "cz c"
+alias gl       "gleam"
+alias gconf    "cd ~/.config/git && nvim config"
+alias la       "ls --icons -A"
+alias lg       "lazygit"
+alias ll       "ls --icons -la"
+alias ls       "eza --icons"
+alias mkd      "mkdir -p"
+alias nv       "nvim"
+alias nvimconf "cd ~/.config/nvim && nvim init.lua"
+alias p        "pnpm"
+alias rm       "gomi"
+alias rmrm     "/bin/rm"
+alias venv     "source ./venv/bin/activate.fish"
+alias wget     "wget --hsts-file='$XDG_DATA_HOME/wget-hsts'"
+alias y        "yarn"
+alias yz       "yazi"
+
+
+# Functions
+function urld
+  echo $argv | nkf -w --url-input
+end
+
+function urle
+  echo $argv | nkf -WwMQ | tr = %
+end
+
+# tar gz
+function tz
+  echo $argv
+end
+
+# zoxide (must be placed in the last line)
+zoxide init fish | source
---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,192 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local mux = wezterm.mux
+
+-- Session manager plugin
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Auto-save session with debounce
+local save_pending = false
+local function save_session()
+	if save_pending then
+		return
+	end
+	save_pending = true
+	wezterm.time.call_after(5, function()
+		local state = resurrect.workspace_state.get_workspace_state()
+		resurrect.state_manager.save_state(state, "session")
+		save_pending = false
+	end)
+end
+
+-- Save when tabs/panes change
+wezterm.on("update-status", save_session)
+
+-- Restore session on startup and maximize
+wezterm.on("gui-startup", function(cmd)
+	local state = resurrect.state_manager.load_state("session", "workspace")
+	if state then
+		resurrect.workspace_state.restore_workspace(state, { relative = true, restore_text = true })
+	else
+		mux.spawn_window(cmd or {})
+	end
+	-- Maximize all windows after restore
+	wezterm.time.call_after(0.5, function()
+		for _, window in ipairs(mux.all_windows()) do
+			window:gui_window():maximize()
+		end
+	end)
+end)
+
+-- Font Settings
+-- config.font = wezterm.font("SauceCodePro Nerd Font")
+-- config.font_size = 12
+config.font = wezterm.font("PixelMplus12", { weight = "Regular" })
+config.font_size = 14
+font = wezterm.font_with_fallback({
+	"SauceCodePro Nerd Font",
+})
+-- config.font_size = 17
+
+-- Suppress missing glyph warnings
+config.warn_about_missing_glyphs = false
+
+-- Color Scheme (WezTerm includes 700+ built-in schemes)
+config.color_scheme = "Catppuccin Mocha"
+
+-- Background Image
+config.background = {
+	{
+		source = {
+			File = wezterm.config_dir .. "/wallpapers/cafe_1_1.jpg",
+		},
+		hsb = { brightness = 0.02 },
+	},
+}
+
+-- Tab Bar Configuration
+config.use_fancy_tab_bar = true
+config.tab_bar_at_bottom = false
+
+-- Key Bindings
+config.keys = {
+	-- Split panes
+	{ key = "d", mods = "CMD", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "d", mods = "CMD|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+
+	-- Navigate panes
+	{ key = "h", mods = "CMD|SHIFT", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "l", mods = "CMD|SHIFT", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ key = "k", mods = "CMD|SHIFT", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "j", mods = "CMD|SHIFT", action = wezterm.action.ActivatePaneDirection("Down") },
+
+	-- Cycle through panes
+	{
+		key = "[",
+		mods = "CMD",
+		action = wezterm.action_callback(function(win, pane)
+			local tab = win:active_tab()
+			local panes = tab:panes_with_info()
+			local active_idx = nil
+			for i, p in ipairs(panes) do
+				if p.is_active then
+					active_idx = i
+					break
+				end
+			end
+			if active_idx then
+				local next_idx = active_idx - 1
+				if next_idx < 1 then
+					next_idx = #panes
+				end
+				panes[next_idx].pane:activate()
+			end
+		end),
+	},
+	{
+		key = "]",
+		mods = "CMD",
+		action = wezterm.action_callback(function(win, pane)
+			local tab = win:active_tab()
+			local panes = tab:panes_with_info()
+			local active_idx = nil
+			for i, p in ipairs(panes) do
+				if p.is_active then
+					active_idx = i
+					break
+				end
+			end
+			if active_idx then
+				local next_idx = active_idx + 1
+				if next_idx > #panes then
+					next_idx = 1
+				end
+				panes[next_idx].pane:activate()
+			end
+		end),
+	},
+
+	-- Close pane
+	{ key = "w", mods = "CMD", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+
+	-- Tab navigation
+	{ key = "1", mods = "CMD", action = wezterm.action.ActivateTab(0) },
+	{ key = "2", mods = "CMD", action = wezterm.action.ActivateTab(1) },
+	{ key = "3", mods = "CMD", action = wezterm.action.ActivateTab(2) },
+	{ key = "4", mods = "CMD", action = wezterm.action.ActivateTab(3) },
+	{ key = "5", mods = "CMD", action = wezterm.action.ActivateTab(4) },
+	{ key = "6", mods = "CMD", action = wezterm.action.ActivateTab(5) },
+	{ key = "7", mods = "CMD", action = wezterm.action.ActivateTab(6) },
+	{ key = "8", mods = "CMD", action = wezterm.action.ActivateTab(7) },
+	{ key = "9", mods = "CMD", action = wezterm.action.ActivateTab(-1) },
+
+	-- Move tabs left/right
+	{ key = "[", mods = "CMD|SHIFT", action = wezterm.action.MoveTabRelative(-1) },
+	{ key = "]", mods = "CMD|SHIFT", action = wezterm.action.MoveTabRelative(1) },
+
+	-- Session management (resurrect)
+	{
+		key = "s",
+		mods = "CMD|SHIFT",
+		action = wezterm.action_callback(function(win, pane)
+			local state = resurrect.workspace_state.get_workspace_state()
+			resurrect.state_manager.save_state(state, "session")
+			win:toast_notification("WezTerm", "Session saved", nil, 2000)
+		end),
+	},
+	{
+		key = "r",
+		mods = "CMD|SHIFT",
+		action = wezterm.action_callback(function(win, pane)
+			local state = resurrect.state_manager.load_state("session", "workspace")
+			if state then
+				resurrect.workspace_state.restore_workspace(state, { relative = true, restore_text = true })
+				win:toast_notification("WezTerm", "Session restored", nil, 2000)
+			end
+		end),
+	},
+
+	-- Quit with save (Cmd+Q)
+	{
+		key = "q",
+		mods = "CMD",
+		action = wezterm.action_callback(function(win, pane)
+			local state = resurrect.workspace_state.get_workspace_state()
+			resurrect.state_manager.save_state(state, "session")
+			win:perform_action(wezterm.action.QuitApplication, pane)
+		end),
+	},
+
+	-- Insert newline without sending (Shift+Enter) using bracketed paste
+	{
+		key = "Enter",
+		mods = "SHIFT",
+		action = wezterm.action.SendString("\x1b[200~\n\x1b[201~"),
+	},
+}
+
+-- Finally, return the configuration to wezterm:
+return config
