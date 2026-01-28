46f9c5788248caf639af9818eaa01028fd9459a9
音㦡 <215371699+med0rei@users.noreply.github.com>
2026-01-28 22:36:58 +0900

    update

--- private_dot_config/nvim/private_lua/private_plugins/treesitter.lua
+++ private_dot_config/nvim/private_lua/private_plugins/treesitter.lua
@@ -2,11 +2,18 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    main = "nvim-treesitter",
-    opts = {
-      ensure_installed = { "gleam" },
-      highlight = { enable = true },
-    },
+    lazy = false,
+    config = function()
+      require("nvim-treesitter").setup({})
+      require("nvim-treesitter").install({ "gleam" })
+    end,
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          pcall(vim.treesitter.start, args.buf)
+        end,
+      })
+    end,
   },
   {
     "windwp/nvim-ts-autotag",
